0523f9e1d18e7f77e3b57058e99275afe086049d
Dron Rathore <dron.rathore@gmail.com>
2017-06-11 20:35:43 +0530

    Fix/issues (#2)
    [Major] Add more methods to API for future plans and fix a mutex lock bug
    * Add Freeze, Unfreeze, Unregister methods for better control of the queue
    * Add EventHandler abstract class to help instances to listen on events
    * Fix a mutex bug
    * Move example file from test folder and rename it
    * Update Readme

---
+++ example/example.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include "../src/main.hpp"
+using namespace std;
+using namespace E2;
+Handle listener(Handle, Handle);
+
+class FsWrap : public E2::EventHandler {
+  private:
+    int identifier; /* inode, fd */
+  public:
+    FsWrap(){
+      this->identifier = -1; /* init state */
+    }
+    void HandleEvent(Handle event, Handle data){
+      /* Trigger the events as they appear */
+      E2::EventData *instance = (E2::EventData *)event;
+      /* switch(instance->name) {case "end": break;} */
+      cout << "File Event: " << *(instance->name) << endl;
+      clear_e2_event(instance)
+    }
+    /*
+      Event handlers to be overriden by extending class
+    */
+    virtual void onData(Handle data){}
+    virtual void onEnd(){}
+    virtual void onStart(){}
+    virtual void onFlush(){}
+    ~FsWrap(){
+
+    }
+};
+
+int main(int argc, char* argv[]){
+  E2::EventMan *man; /* you can share this instance with other threads */
+  man = new E2::EventMan();
+  man->Listen("push", &listener);
+  
+  Handle* data = (Handle*)new int(123);
+  man->Trigger("push", data);
+  man->Trigger("push", nil);
+  man->Trigger("push", nil);
+  /* If event is not present then its a noop */
+  man->Trigger("lol", nil);
+  // call all fs/net and other threads before this
+  // this is the end marker which will block the code
+  auto FsQueue = new E2::EventMan();
+  auto fsObject = new FsWrap();
+  /* todo: Add thread pool */
+  /* Till then you can namespace events with inode values */
+  /* or fd in case of net connection */
+  FsQueue->Listen("onstart", fsObject);
+  /* you can have functions as well as EventHandler classes */
+  /* listening on the same event */
+  FsQueue->Listen("onstart", &listener);
+  FsQueue->Trigger("onstart", nil);
+  /* Exit method suspends the existing event queue thread
+    flushes all the events and event datas that are queued
+
+    man->Exit();
+  */
+  FsQueue->Unregister("onstart");
+  int *sample = new int(0xfAAAAA);
+  /* None will be triggered */
+  FsQueue->Trigger("onstart", (Handle*)sample);
+  FsQueue->Join();
+  man->Exit();
+  if (man->isAlive())
+    man->Join();
+  return 0;
+}
+
+Handle listener(Handle event, Handle data){
+  E2::EventData *e = (E2::EventData *)event;
+  cout << "Event Triggered" << std::endl;
+  cout << "Event name=> " << *(e->name) << std::endl;
+  if (data != nil){
+    cout << "The data is an integer: " << *((int*)data) <<endl;
+  }
+  /* 
+    use the helper to clear event data if you want to clear
+    the whole event object with data
+    if not, then just call clear_e2_event_name(e) and clear_e2_event_obj(e)
+    note: Don't call both together
+  */
+  // clear_e2_event_name(e) // delete event->name
+  // clear_e2_event_obj(e) // delete event
+  clear_e2_event(e) // this call deletes data, name and event itself
+  return nil;
+}
--- src/main.hpp
+++ src/main.hpp
@@ -24,26 +24,73 @@
 /* delete std::string instance of event name */
 #ifndef clear_e2_event_name
   #define clear_e2_event_name(event) \
-            if (event->name != nil)\
+            if (event->name != nil)  \
               delete event->name;
 #endif
 /* delete struct event */
 #ifndef clear_e2_event_obj
   #define clear_e2_event_obj(event) \
-            if (event != nil)\
+            if (event != nil)       \
               delete event;
 #endif
 
 /* completley delete the event object */
 #ifndef clear_e2_event
-  #define clear_e2_event(event) \
-          if (event != nil){ \
+  #define clear_e2_event(event)      \
+          if (event != nil){         \
             if (event->name != NULL) \
-              delete event->name; \
-            if (event->data != nil) \
-              delete event->data; \
-            delete event; \
+              delete event->name;    \
+            if (event->data != nil)  \
+              delete event->data;    \
+            delete event;            \
           }
+#endif
+
+#ifndef ReturnIfNoListeners
+  #define ReturnIfNoListeners(type1, type2, name)                           \
+    if (this->type1##_map[name] == nil && this->type2##_map[name] == nil) { \
+      this->type1##_map.erase(name);                                        \
+      this->type2##_map.erase(name);                                        \
+      this->self.unlock();                                                  \
+      return 0;                                                             \
+    }
+#endif
+
+#ifndef TriggerListeners
+  #define TriggerListeners(type, event_name, key)       \
+    if (this->type##_map[event_name] != nil) {          \
+      auto listeners = *(this->type##_map[event_name]); \
+      for(auto el : listeners){                         \
+        auto e = new EventData();                       \
+        e->name = new std::string(event_name);          \
+        e->key = el;                                    \
+        e->data = data;                                 \
+        this->event_queue->push(e);                     \
+        ++count;                                        \
+      }                                                 \
+    } else {                                            \
+      this->type##_map.erase(event_name);               \
+    }
+#endif
+
+#ifndef ClearEvent
+  #define ClearEvent(type, event_name)           \
+    if (this->type##_map[event_name] != nil) {   \
+      auto _list = this->type##_map[event_name]; \
+      _list->clear();                            \
+    }                                            \
+    this->type##_map.erase(event_name);
+#endif
+
+#ifndef ClearListener
+  #define ClearListener(type, event_name, handler)  \
+    if (this->type##_map[event_name] != nil) {      \
+      auto _list = this->type##_map[event_name];    \
+      _list->erase(std::remove(_list->begin(),      \
+         _list->end(), handler), _list->end());     \
+    } else {                                        \
+      this->type##_map.erase(event_name);           \
+    }
 #endif
 
 namespace E2{
@@ -51,12 +98,23 @@ namespace E2{
   typedef void* Handle;
   /* signature of our thread function */
   typedef Handle (*EventCallbackHandle)(Handle, Handle);
+  
+  class EventHandler{
+    public:
+      virtual void HandleEvent(E2::Handle, E2::Handle){}
+      virtual ~EventHandler(){}
+  };
+
   typedef std::map<std::string, std::vector<EventCallbackHandle>*> EventsMap;
+  typedef std::map<std::string, std::vector<EventHandler*>*> HandlerInstance;
+  
   typedef struct edata{
     std::string *name;
     EventCallbackHandle callback;
+    EventHandler* instance;
     Handle *data;
   }EventData;
+
   class EventQueue{
     private:
       std::thread *thread;
@@ -76,6 +134,7 @@ namespace E2{
   class EventMan{
     private:
       EventsMap event_map;
+      HandlerInstance instance_map;
       Handle initQueue();
       EventQueue *event_queue;
       bool _isAlive;
@@ -83,9 +142,16 @@ namespace E2{
     public:
       EventMan();
       void Listen(std::string event_name, EventCallbackHandle listener);
+      void Listen(std::string event_name, EventHandler *instance);
       int Trigger(std::string event_name, Handle *data);
+      void Unregister(std::string event_name);
+      void Unregister(std::string event_name, EventCallbackHandle listener);
+      void Unregister(std::string event_name, EventHandler *instance);
+      void UnregisterAll();
       void Exit();
       void Join();
+      void Freeze();
+      bool Unfreeze();
       bool isAlive();
       ~EventMan();
   };
